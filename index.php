d89f9fbc9e9c8c6d453dda1ee014356076a05932
Elric Barkey <ebarkey2@mail.greenriver.edu>
2020-04-24 16:40:00 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the auto load file
+require_once ("vendor/autoload.php");
+
+//instantiate the F3 Base class
+$f3 = Base::instance();
+
+//default route
+$f3->route('GET /', function(){
+    echo "<h1>Pet Home</h1>";
+});
+
+//run f3
+$f3->run();
