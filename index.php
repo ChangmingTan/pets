795dea444c456dcc96d9faf5ab8d7d9084de57f4
Changming Tan <ctan9@mail.greenriver.edu>
2020-04-24 16:45:54 -0400

    Added home.html and updated index.php

--- index.php
+++ index.php
@@ -4,14 +4,17 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //require the auto load file
-require_once ("vendor/autoload.php");
+require_once("vendor/autoload.php");
 
 //instantiate the F3 Base class
 $f3 = Base::instance();
 
 //default route
-$f3->route('GET /', function(){
-    echo "<h1>Pet Home</h1>";
+$f3->route('GET /', function () {
+    //echo "<h1>Pet Home</h1>";
+
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //run f3
